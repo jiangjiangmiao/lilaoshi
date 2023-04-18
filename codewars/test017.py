0bbb6eeebdc228144dbbdae3068c906a01942c13
zyl <1103987935@qq.com>
2023-04-18 17:06:15 +0800

    update

--- codewars/test017.py
+++ codewars/test017.py
@@ -1,14 +1,13 @@
 # In this kata, you need to write a function that takes a string and a letter as input
 # and then returns the index of the nth occurrence of that letter in the string. If there is no such letter in the string,
-# then the function should return -1. If the letter occurs only once in the string, then -1 should also be returned.
-#
+# then the function should return -1.
 # Examples:
 #
 # second_symbol('Hello world!!!','l', 2) --> 3
 # second_symbol('Hello world!!!', 'A', 1) --> -1
 
 def nth_symbol(s, symbol, n):
-    if s.count(symbol) == 0 or s.count(symbol) == 1:
+    if s.count(symbol) == 0:
         return -1
     if s.count(symbol) >= n:
         new_s = list(s)
@@ -26,3 +25,4 @@ print(nth_symbol('Hello world!!!', 'o', 1))
 print(nth_symbol('Hello world!!!', 'A', 1))
 print(nth_symbol('', 'q', 1))
 print(nth_symbol('Hello Lucy How are you I am fine thank you', 'e', 3))
+print(nth_symbol('Hello', 'o', 1))
--- codewars/test018.py
+++ codewars/test018.py
@@ -20,7 +20,7 @@ def calc(txt):
     symbol = txt[space + 1]
     left = txt[:space]
     right = txt[space + 3:]
-    if left.count('.') < right.count('.'):
+    if left.count('.') < right.count('.') and symbol == '-' or left.count('.') < right.count('.') and symbol == '/':
         return "' '"
     if symbol == "+":
         return (left.count('.') + right.count('.')) * "."
@@ -38,3 +38,4 @@ def calc(txt):
 # print(calc("..... * ..."))
 print(calc(". // .."))
 print(calc("..... // ."))
+print(calc("..... + ..............."))
